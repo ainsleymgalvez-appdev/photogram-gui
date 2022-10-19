935cf389d415efcb10714d5b1fabbec2d3025621
Ainsley M Galvez <galvezainsley@gmail.com>
2022-10-19 17:10:20 +0000

    finished delete button

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -11,6 +11,30 @@ class PhotosController < ApplicationController
 
   end
 
+  def show
+
+    get_id = params.fetch("photo_id")
+
+    @match_photo = Photo.where({:id => get_id }).at(0)
+
+    # @match_comment = Comment.where({ :photo_id => get_id })
+
+    render({:template => "photo_templates/show_photo.html.erb"})
+
+  end
+
+  def delete
+    photo_id = params.fetch("photo_id")
+
+    pick_photo = Photo.where({:id => photo_id}).at(0)
+    
+    pick_photo.destroy
+
+    #render({:template => "photo_templates/goodbye.html.erb"})
+
+    redirect_to("/photos")
+
+  end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,21 @@
 Rails.application.routes.draw do
+
+ # HOMEPAGE
+
  get("/", { :controller => "users", :action => "index" })
+
+ #USERS
 
   get("/users", { :controller => "users", :action => "index" })
 
  get("/users/:username", {:controller => "users", :action => "show"})
 
- get("/photos", {:controller => "photos", action: "index"})
+ # PHOTOS
+
+ get("/photos", {:controller => "photos", :action => "index"})
+ get("/photos/:photo_id", {:controller => "photos", :action => "show"})
+
+ # DELETE PHOTO
+ get("/delete_photo/:photo_id", {:controller => "photos", :action => "delete"})
 
 end
